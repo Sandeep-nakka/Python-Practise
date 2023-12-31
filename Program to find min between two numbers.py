87354866240ac3adda52e74e7b41b9773eb8b780
Sandeep Yadav <56767489+Sandeep-nakka@users.noreply.github.com>
2023-12-31 09:17:14 +0530

    Create Program to find min between two numbers.py

---
+++ Program to find min between two numbers.py
@@ -0,0 +1,8 @@
+#program to find min between two numbers
+
+Number1 = int(input("Enter the Number1 : "))
+Number2 = int(input("Enter the Number 2 : "))
+if Number2>Number1:
+    print("{} in Minimum Number between {} and {}".format(Number1,Number1,Number2))
+else:
+    print("{} in Minimum Number between {} and {}".format(Number2, Number1, Number2))
